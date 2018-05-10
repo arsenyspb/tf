1d7604aa2799fa9629e4bae9ba0ffd6534ba9d1e
root <root@arsenyspb2.mylabserver.com>
2018-05-10 09:25:09 +0000

    After testing

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {}
+variable "aws_profile" {}
+data "aws_availability_zones" "available" {}
+variable "vpc_cidr" {}
+
+#next one is a map so we could look up as a dictionary
+variable "cidrs" {
+  type = "map"
+}
+
+variable "localip" {}
+variable "domain_name" {}
+
+variable "db_instance_class" {}
+variable "dbname" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+
+variable "key_name" {}
+variable "public_key_path" {}
+variable "dev_instance_type" {}
+variable "dev_ami" {}
+variable "elb_healthy_threshold" {}
+variable "elb_unhealthy_threshold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
+variable "asg_max" {}
+variable "asg_min" {}
+variable "asg_grace" {}
+variable "asg_hct" {}
+variable "asg_cap" {}
+variable "lc_instance_type" {}
+variable "delegation_set" {}
+
